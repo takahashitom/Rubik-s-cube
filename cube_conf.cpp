81b0b98005e0fd5e26e4683ef5c44321a111f9d4
takahashi <tkhs.tmhr39@gmail.com>
2024-05-20 17:23:01 +0900

    関数cube_dispおよびco, eoの初期状態の修正

--- cube_conf.cpp
+++ cube_conf.cpp
@@ -7,16 +7,16 @@ void cube_disp(Cube C)
     C.cp_disp();
     C.co_disp();
     C.ep_disp();
-    C.ep_disp();
+    C.eo_disp();
     cout << "step: " << C.get_step() << endl;
 }
 
 int main()
 {
     int init_cp[sizeC] = {0, 1, 2, 3, 4, 5, 6, 7};
-    int init_co[sizeC] = {0, 1, 2, 3, 4, 5, 6, 7};
+    int init_co[sizeC] = {0, 0, 0, 0, 0, 0, 0, 0};
     int init_ep[sizeE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    int init_eo[sizeE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int init_eo[sizeE] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     Cube C(init_cp, init_co, init_ep, init_eo, 0);
 
