81da6f774be1feb80f5ad7bf33237fb3465bcdd8
takahashi <tkhs.tmhr39@gmail.com>
2024-05-21 16:33:01 +0900

    時間測定機能の追加

--- dfs.cpp
+++ dfs.cpp
@@ -1,6 +1,8 @@
 #include "cube.h"
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 void cube_disp(Cube C)
 {
@@ -12,6 +14,8 @@ void cube_disp(Cube C)
 
 int main()
 {
+    auto t0 = high_resolution_clock::now();
+
     int init_cp[sizeC] = {0, 1, 2, 3, 4, 5, 6, 7};
     int init_co[sizeC] = {0, 0, 0, 0, 0, 0, 0, 0};
     int init_ep[sizeE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
@@ -23,10 +27,13 @@ int main()
 
     x.start_search(C, 20);
 
-    C = C.apply_move(l_move);
-    C = C.apply_move(r_move);
-    C = C.apply_move(ur_move);
     C = C.apply_move(rr_move);
+    C = C.apply_move(f2_move);
+    C = C.apply_move(d2_move);
+    C = C.apply_move(l_move);
 
     y.start_search(C, 20);
+
+    auto t1 = high_resolution_clock::now();
+    cout << duration_cast<milliseconds>(t1 - t0).count() / 1000. << " 秒\n";
 }
