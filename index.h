9607b478f5ad1245f5be96691c79e234e618b2a3
takahashi <tkhs.tmhr39@gmail.com>
2024-07-18 22:47:42 +0900

    関数の説明の追加

--- index.h
+++ index.h
@@ -9,20 +9,28 @@ int co_to_index(Cube state);
 // indexからcoを計算する関数
 std::vector<int> index_to_co(int index);
 
+// eoからindexを計算する関数
 int eo_to_index(Cube state);
 
+// indexからeoを計算する関数
 std::vector<int> index_to_eo(int index);
 
+// e列エッジの組み合わせからindexを計算する関数
 int combination_to_index(std::vector<int> combination);
 
+// indexからe列エッジの組み合わせを計算する関数
 std::vector<int> index_to_combination(int index);
 
+// cpもしくはud面のepからindexを計算する関数
 int cp_ep_to_index(std::vector<int> parts);
 
+// indexからcpもしくはud面のepを計算する関数
 std::vector<int> index_to_cp_ep(int index);
 
+// e列のepからindexを計算する関数
 int e_ep_to_index(std::vector<int> parts);
 
+// indexからe列のepを計算する関数
 std::vector<int> index_to_e_ep(int index);
 
 #endif
